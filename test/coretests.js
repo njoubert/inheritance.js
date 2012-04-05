575944cb24209cf3f60139403c21efdc4fbceb53
Niels Joubert <njoubert@gmail.com>
2012-04-05 14:45:28 -0700

    Ordering of requirejs modules makes things load correctly. tests now split correctly

--- test/coretests.js
+++ test/coretests.js
@@ -3,29 +3,56 @@ define(
   
   //Here we list the modules this test depends on.
   //We are testing the inheritance library...
-  ['../inheritance', 'lib/jasmine-1.2.0.rc3/jasmine.js', 'lib/jasmine-1.2.0.rc3/jasmine-html.js'],
+  ['order!../inheritance', 'order!lib/jasmine-1.2.0.rc3/jasmine.js', 'order!lib/jasmine-1.2.0.rc3/jasmine-html.js'],
   
   //Here we code the actual Jasmine tests,
   //with the library passed in.
   function(Base) {
+    
+    window.Base = Base;
 
+
+    //Test classes
+    var Mammal = Base.extend({
+      init: function(name) {
+        this.name = name;
+      }
+    }, "Mammal"); 
+
+    var Snake = Mammal.extend({
+      init: function(name) {
+        this.parent.init.call(this, name);
+      },
+      slither: function() {
+        return this.name + " says sssSSSSssss";
+      }
+    });
+
+
+    //Tests
     describe("Base", function() {
-
-      var Mammal = Base.extend({
-        init: function(name) {
-          this.name = name;
-        }
-      }, "Mammal"); 
-
-      var Snake = Mammal.extend({
-        init: function(name) {
-          this.parent.init.call(this, name);
-        },
-        slither: function() {
-          return this.name + " says sssSSSSssss";
-        }
+      
+      it("should have extend function", function() {
+        expect(typeof Base.extend === "function").toBeTruthy();
       });
+      
+      it("should take 2 arguments", function() {
+        expect(Base.extend.length === 2).toBeTruthy();
+      })
 
+      it("should accept having just 1 argument", function() {
+        expect( function() {Base.extend({})} ).not.toThrow(new Error("Illegal arguments"));
+      })
+      
+    });
+    
+    describe("Mammal", function() {
+      
+      
+    });
+    
+    describe("Snake", function() {
+      
       var myMammal;
       var mySnake;
 
@@ -34,10 +61,7 @@ define(
         mySnake = new Snake("Billybob");
       });
 
-      it("should have snake instanceof mammal", function() {
-        expect(mySnake instanceof Mammal).toBeTruthy();
-      });
-
+      
     });        
     
   }
