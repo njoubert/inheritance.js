63f0cd920b273dc64f5f1ddbf37d05261d5f6c23
Niels Joubert <njoubert@gmail.com>
2012-04-05 17:24:23 -0700

    tests for correct number of arguments to init

--- test/coretests.js
+++ test/coretests.js
@@ -20,11 +20,12 @@ define(
     }, "Mammal"); 
 
     var Snake = Mammal.extend({
-      init: function(name) {
+      init: function(name, sound) {
         this.parent.init.call(this, name);
+        this.sound = sound;
       },
       slither: function() {
-        return this.name + " says sssSSSSssss";
+        return this.name + " says " + this.sound;
       }
     });
 
@@ -58,9 +59,41 @@ define(
         expect(typeof Mammal === "function").toBeTruthy();
       });
 
-      it("not using a constructor should still work", function() {
-        var didntUseNew = Mammal();
-        expect(typeof didntUseNew === "object").toBeTruthy();
+      it("not using new should cause exception", function() {
+        expect( function() { 
+            var didntUseNew = Mammal("MyName"); 
+          }).toThrow(
+            { 
+              name: "ClassError", 
+              message: "Called constructor without using 'new'."
+            });
+      });
+      
+      it("should be an instance of Mammal", function() {
+        expect(myMammal instanceof Mammal).toBeTruthy();
+      });
+      
+      it("should be an instance of Base", function() {
+        expect(myMammal instanceof Base).toBeTruthy();
+      });
+      
+      it("should not be an instance of Snake", function() {
+        expect(myMammal instanceof Snake).toBeFalsy();
+      });
+      
+      it("should have the name set correctly", function() {
+        expect(myMammal.name === "Job").toBeTruthy();
+      });
+      
+      it("should not work if you give it the wrong number of arguments", function() {
+        expect( function() { 
+            var wrongargs = new Mammal(); 
+          }).toThrow(
+            { 
+              name: "ClassError", 
+              message: "Called constructor with incorrect number of arguments."
+            });
+        
       });
       
     });
@@ -72,10 +105,77 @@ define(
 
       beforeEach(function() {
         myMammal = new Mammal("Job");
-        mySnake = new Snake("Billybob");
+        mySnake = new Snake("Billybob", "sssSSSsss");
       });
 
+      it("should be a constructor function", function() {
+        expect(typeof Snake === "function").toBeTruthy();
+      });
+
+      it("not using new should cause exception", function() {
+        expect( function() { 
+            var didntUseNew = Snake("MyName"); 
+          }).toThrow(
+            { 
+              name: "ClassError", 
+              message: "Called constructor without using 'new'."
+            });
+      });
       
+      it("should be an instance of Mammal", function() {
+        expect(mySnake instanceof Mammal).toBeTruthy();
+      });
+      
+      it("should be an instance of Base", function() {
+        expect(mySnake instanceof Base).toBeTruthy();
+      });
+      
+      it("should be an instance of Snake", function() {
+        expect(mySnake instanceof Snake).toBeTruthy();
+      });
+      
+      it("should have the name set correctly", function() {
+        expect(mySnake.name === "Billybob").toBeTruthy();
+      });
+
+      it("should have the sound set correctly", function() {
+        expect(mySnake.sound === "sssSSSsss").toBeTruthy();
+      });
+      
+      it("should have a slither function", function() {
+        expect(typeof mySnake.slither === "function").toBeTruthy();
+      });
+
+      it("should have a slither function on the constructor's prototype", function() {
+        expect(typeof Snake.prototype.slither === "function").toBeTruthy();
+      });
+
+      it("should have a slither function on the prototype", function() {
+        expect(typeof mySnake.__proto__.slither === "function").toBeTruthy();
+      });
+      
+      it("the constructor prototype's slither function should be equal to the object's slither function", function() {
+        expect(Snake.prototype.slither === mySnake.__proto__.slither).toBeTruthy();
+      });
+      
+      it("the slither function should return the right value", function() {
+        expect(mySnake.slither() === "Billybob says sssSSSsss").toBeTruthy();
+      });
+      
+      it("should not work if you give it the wrong number of arguments", function() {
+        expect( function() { 
+            var wrongargs = new Snake("MyName"); 
+          }).toThrow(
+            { 
+              name: "ClassError", 
+              message: "Called constructor with incorrect number of arguments."
+            });
+        
+      });
+      
+      
+      
+            
     });        
     
   }
